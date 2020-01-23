8c1b31b39d9288ff78f7dea2580cba7e7bf364a8
bstefansen <54404576+bstefansen@users.noreply.github.com>
2020-01-23 17:55:13 -0500

    Updates

---
+++ precache-manifest.0ad3a8c5c3968c78937aeb88d25592e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f22dc2ddd70422227d5222e6f4138d",
+    "url": "/PomodoroClock/index.html"
+  },
+  {
+    "revision": "c1dc02068c30b5d38c41",
+    "url": "/PomodoroClock/static/css/main.d882b92c.chunk.css"
+  },
+  {
+    "revision": "180adb0ccefdc3644e1b",
+    "url": "/PomodoroClock/static/js/2.3fe75f1d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/PomodoroClock/static/js/2.3fe75f1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1dc02068c30b5d38c41",
+    "url": "/PomodoroClock/static/js/main.bce267d7.chunk.js"
+  },
+  {
+    "revision": "95bee7b7a1d154602eac",
+    "url": "/PomodoroClock/static/js/runtime-main.5117bd55.js"
+  }
+]);
